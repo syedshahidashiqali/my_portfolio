8ec34079110c0232684a692a2bfaf18ca818cabe
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-07 16:39:37 +0500

    complete left side of intro component
    add animation and design left part of intro section in intro component

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,7 +3,29 @@ import "./Intro.css";
 function Intro() {
   return (
     <div className="intro">
-      Intro
+      <div className="introLeft">
+        <div className="introLeftWrapper">
+          <h2 className="introLeftDesc">Hello, My name is</h2>
+          <h1 className="introLeftName">Shahid Ali</h1>
+          <div className="introLeftTitles">
+            <div className="introLeftItemWrapper">
+            <div className="introLeftItem">Web Developer</div>
+            <div className="introLeftItem">UI / UX Developer</div>
+            <div className="introLeftItem">ReactJS Developer</div>
+            <div className="introLeftItem">NodeJS Developer</div>
+            <div className="introLeftItem">MERN stack Developer</div>
+            <div className="introLeftItem">Frontend Developer</div>
+            <div className="introLeftItem">Backend Developer</div>
+            </div>
+          </div>
+          <p className="introDetail">
+          I design and develop services for customers of all sizes, 
+          specializing in creating stylish, modern websites, web services 
+          and online stores.
+          </p>
+        </div>
+      </div>
+      <div className="introRight">Intro Right</div>
     </div>
   );
 }
