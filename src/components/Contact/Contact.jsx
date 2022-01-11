a3cca2ad1ab34e00dd2b4344e8ded21a148f0352
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-11 14:54:18 +0500

    attach ref to needed componenrts

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,8 +2,18 @@ import "./Contact.css";
 import PhoneImg from "../../images/phone.png";
 import EmailImg from "../../images/email.png";
 import LocationImg from "../../images/address.png";
+import { useRef } from "react";
 
 function Contact() {
+  const formRef = useRef();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(12, formRef);
+    console.log(13, formRef.current);
+    console.log(14, formRef.current[0]);
+    console.log(15, formRef.current[0].value);
+  };
   return (
     <div className="contact">
       <div className="contactBg"></div>
@@ -42,7 +52,7 @@ function Contact() {
             <b>What’s your story?</b> Get in touch. Always available for freelancing 
             if the right project comes along. me.
           </p>
-          <form>
+          <form ref={formRef} onSubmit={handleSubmit}>
             <input type="text" placeholder="Name" name="user_name" />
             <input type="text" placeholder="Subject" name="user_subject" />
             <input type="text" placeholder="Email" name="user_email" />
