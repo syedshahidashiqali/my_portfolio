12ef53467c6b130ad4419c65ffcb0c481fd89297
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-03-22 20:24:25 +0500

    add new project data

--- src/data.js
+++ src/data.js
@@ -1,104 +1,110 @@
 export const projects = [
   {
+    id: 18,
+    img: "https://drive.google.com/uc?export=view&id=1W0G5MyFYPTjEimo5yb0BQP5Bx7n79K70",
+    link: "https://nextjs-food-order-app.vercel.app/",
+    alt: "MoyenXpress E-commerce",
+  },
+  {
     id: 16,
     img: "https://drive.google.com/uc?export=view&id=1IgVH3Md_qPkpTiJgu29bUxyVWQNoPbUc",
     link: "https://nextjs-food-order-app.vercel.app/",
-    alt: "NEXT JS E-commerce" 
+    alt: "NEXT JS E-commerce",
   },
   {
     id: 17,
     img: "https://drive.google.com/uc?export=view&id=180USVklyAaW7MC0FOasABDUp189XJ1uq",
     link: "https://react-budget-app-by-shahid.surge.sh/",
-    alt: "Budget app with React" 
+    alt: "Budget app with React",
   },
   {
     id: 1,
     img: "https://drive.google.com/uc?export=view&id=1FIsvRZe_yTOjY5Dzrj5iUs5UBiWWw995",
     link: "https://facebook-clone-by-shahid.surge.sh/",
-    alt: "Facebook Clone"
+    alt: "Facebook Clone",
   },
   {
     id: 2,
     img: "https://drive.google.com/uc?export=view&id=1WKr0MgAcCwM63mxnWFc8X-FHNaDHuTLt",
     link: "https://reactjs-face-detection-app-by-shahid.surge.sh/",
-    alt: "Face Detection App"
+    alt: "Face Detection App",
   },
   {
     id: 3,
     img: "https://drive.google.com/uc?export=view&id=1wL4WFLVcIYJt495uDIIfU_X0cKTo0NOr",
     link: "https://reactjs-admin-dashboard-by-shahid.surge.sh/",
-    alt: "Admin Dashboard"
+    alt: "Admin Dashboard",
   },
   {
     id: 4,
     img: "https://drive.google.com/uc?export=view&id=1qC7LAxcowABphr0LLDDR5nPXYf61QGy_",
     link: "http://coronavirus-tracker-by-shahid.surge.sh/",
-    alt: "Covid19 Tracker"
+    alt: "Covid19 Tracker",
   },
   {
     id: 5,
     img: "https://drive.google.com/uc?export=view&id=13o35JTkqGvSgFLOiC0oAVdkbGpkoz3kK",
     link: "https://reactjs-portech-portfolio-clone-by-shahid.surge.sh/",
-    alt: "Portech Portfolio clone"
+    alt: "Portech Portfolio clone",
   },
   {
     id: 6,
     img: "https://drive.google.com/uc?export=view&id=10LYAm80yrFtMXL-BTYiOMXhaL2wtgtby",
     link: "http://expense-tracker-app-by-shahid.surge.sh/",
-    alt: "Expense Tracker App"
+    alt: "Expense Tracker App",
   },
   {
     id: 7,
     img: "https://drive.google.com/uc?export=view&id=1NvyaieWTr2HLBXqFLaXGvSaSupSJ7iMQ",
     link: "https://react-calculator-app-by-shahid.surge.sh/",
-    alt: "Calculator App"
+    alt: "Calculator App",
   },
   {
     id: 8,
     img: "https://drive.google.com/uc?export=view&id=1yGgIIBoA4XIoiS---99ptIs7OzaVPsjk",
     link: "https://drive.google.com/file/d/164DRVd0QYxeDzzXLmh3DYEuJdItWYmv6/view?usp=sharing",
-    alt: "Todo Mobile App"
+    alt: "Todo Mobile App",
   },
   {
     id: 9,
     img: "https://drive.google.com/uc?export=view&id=1d1sABFudt_OCDYI7ebjD5ihX8i3PSHFD",
     link: "http://tinder-clone-by-shahid.surge.sh/",
-    alt: "Todo Mobile App"
+    alt: "Tinder clone",
   },
   {
     id: 10,
     img: "https://drive.google.com/uc?export=view&id=1rtTLhUnHBYO37qFvLFgzuK7A97IDxrEF",
     link: "http://shoes-store-by-shahid.surge.sh/",
-    alt: "Shoes Store"
+    alt: "Shoes Store",
   },
   {
     id: 11,
     img: "https://drive.google.com/uc?export=view&id=1p1j0h1Z1b5Yv1CkAayArtpctHwcdPct4",
     link: "https://saylaniweb258123.surge.sh/",
-    alt: "Welfare website clone"
+    alt: "Welfare website clone",
   },
   {
     id: 12,
     img: "https://drive.google.com/uc?export=view&id=1HNgrgzO0ZWbICi6bPrswHAleAletAtpA",
     link: "https://ecommerce-product-page-by-shahid.surge.sh/",
-    alt: "E-Commerce page"
+    alt: "E-Commerce page",
   },
   {
     id: 13,
     img: "https://drive.google.com/uc?export=view&id=1ZRgUmT1ce18HbSrRyq5bwpanNRs28z8D",
     link: "https://pong-clone-by-shahid.surge.sh/",
-    alt: "Pong Game Clone"
+    alt: "Pong Game Clone",
   },
   {
     id: 14,
     img: "https://drive.google.com/uc?export=view&id=12PysczJxTmjMvTlAKYr-br2b-LAw26jY",
     link: "https://syedshahidashiqali.github.io/spinning-dots-animation/",
-    alt: "Spinning Dots Animation"
+    alt: "Spinning Dots Animation",
   },
   {
     id: 15,
     img: "https://drive.google.com/uc?export=view&id=1VDlb0SI1T4J6X0mP0z2vihAdWTZHKmzr",
     link: "http://project-4a-waapi-by-shahid.surge.sh/",
-    alt: "car animation WAAPI"
+    alt: "car animation WAAPI",
   },
 ];
